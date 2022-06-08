89bae5878e7b60e8c3888125c706d306b0cea737
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-06-08 23:22:06 +0700

    update entities

--- src/entities.py
+++ src/entities.py
@@ -5,38 +5,50 @@ from random import randint
 class Dice:
     def roll(self) -> int:
         return randint(1, 6)
-
-
 class Player:
-    def __init__(self, id: 'int') -> None:
+    def __init__(self, id: 'str', name: 'str') -> None:
         self.__id = id
+        self.__name = name
 
-    def get_id(self) -> int:
+    def get_id(self) -> str:
         return self.__id
 
-class Character:
-    def __init__(self, player_id: 'str') -> None:
-        self.__player_id = player_id
-
-    def get_player_id(self) -> 'int':
-        return self.__player_id
-
-class CharacterMovementType(Enum):
-    BIASA = 1
-    KEPLESET = 2
-    NAIK_TANGGA = 3
-
-class CharacterMovement:
-    def __init__(self, type: 'CharacterMovementType', destination: 'int') -> None:
-        self.__type = type
-        self.__destination = destination
-
-    def get_type(self) -> 'CharacterMovementType':
-        return self.__type
-
-    def get_destination(self) -> int:
-        return self.__destination
+    def get_name(self) -> str:
+        return self.__name
 
 class Board:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, players: 'list[Player]') -> None:
+        self.__players = players
+        self.__size = 10
+        self.__snakes = { 4:2, 8:5 }
+        self.__ladders = { 3:5, 7:9 }
+        self.__playerPosition = {}
+        for player in players:
+            self.__playerPosition[player.get_id()] = 0
+
+    def getPlayer(self, playerId: 'str') -> Player:
+        for player in self.__playerPosition:
+            if player.get_id() == playerId:
+                return player
+
+    def getPlayerPosition(self, playerId: 'str') -> int:
+        return self.__playerPosition.get(playerId)
+
+    def isSnake(self, position: 'int'):
+        return self.__snakes.get(position) != None
+
+    def isLadder(self, position: 'int'):
+        return self.__ladders.get(position) != None
+
+    def movePlayer(self, player: 'Player', diceResult: 'int') -> None:
+        dest = 0
+        position = self.getPlayerPosition(player.get_id()) + diceResult
+
+        if(self.isSnake(position)):
+            dest = self.__snakes.get(position)
+        elif(self.isLadder(position)):
+            dest = self.__ladders.get(position)
+        elif(position > self.__size):
+            dest = 2 * self.__size - position
+
+        self.__playerPosition[player.get_id()] = dest
